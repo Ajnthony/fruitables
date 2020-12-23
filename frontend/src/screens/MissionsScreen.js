eb95a1ec754b9395844ccfa666733e24d540a458
Vegmot <anthony.yh.jang@gmail.com>
2020-12-22 21:15:07 -0800

    missions page done

--- frontend/src/screens/MissionsScreen.js
+++ frontend/src/screens/MissionsScreen.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import FormContainer from '../components/FormContainer';
 
 const MissionsScreen = () => {
   return (
-    <div>
-      <h1>You are on Our Missions page.</h1>
-    </div>
+    <FormContainer>
+      <Row className='text-center'>
+        <Col>
+          <h1>OUR MISSIONS</h1>
+          <p>
+            Fruitables' top priority is customer satisfaction, followed by the
+            quality of products.
+          </p>
+          <hr />
+          <h5>Customer experience</h5>
+          <p>
+            We guarantee that no customers will ever be dissatisfied,
+            discouraged or uncomfortable while shopping with us.
+          </p>
+          <br />
+          <h5>Quality</h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+            repellendus ab dolore fuga veniam ea, architecto eveniet officiis
+            quibusdam ipsum!
+          </p>
+          <br />
+          <h5>Integrity</h5>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores
+            unde explicabo eos.
+          </p>
+          <br />
+          <h5>Honesty</h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, velit
+            recusandae ullam libero numquam ex est.
+          </p>
+          <br />
+          <h5>Respect</h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Voluptatibus laudantium maiores sint ad dolore deserunt qui ipsum
+            nemo, voluptates vero?
+          </p>
+          <br />
+          <h5>Teamwork</h5>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab impedit
+            totam laborum, atque omnis laboriosam officia est cupiditate.
+          </p>
+          <br />
+          <h5>Responsibility</h5>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+            sint quae possimus ipsum expedita quis laboriosam ratione?
+          </p>
+        </Col>
+      </Row>
+    </FormContainer>
   );
 };
 
